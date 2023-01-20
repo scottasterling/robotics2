a5a9d136cc362436ae3518da431800e1db1cf750
Daryl Hampton <hamptond@laurelbrook.org>
2023-01-20 09:23:45 -0500

    configure

---
+++ everything.py
@@ -0,0 +1,6 @@
+name1 = input("what is your name ")
+friend1 = input("what is your friends name ")
+hobby1 = input("what is your favorite hobby ")
+food1 = input("what is your favorite food ")
+print(" one day your friend " + friend1 + " was "+hobby1+ " after "+ hobby1 + + friend1 + " invited " +name1+ " out for " + food1 +" the end ")
+input= ("press  ENTER to exit")
