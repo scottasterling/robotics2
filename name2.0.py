f53f47046564fcd6c0b6068963afa1d1f33a17f4
Darryl Hampton <hamptond@laurelbrook.org>
2023-01-20 09:17:07 -0500

    first commit

---
+++ name2.0.py
@@ -0,0 +1,6 @@
+object1 = input("enter an object: ")
+name1 = input("Enter a name: ")
+verb1 = input("Enter a verb: ")
+print ("one day " + name1 + " was playing on the beach and then saw a " + object1 +", picked it up and " 
++ verb1 + " home ")
+input("press  ENTER to exit")
